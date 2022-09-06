af46cf1bb2c7da7d02dae5a8044e9db519592df0
Desmond Agbesi <buzzbigmind@gmail.com>
2022-09-06 14:37:26 +0000

    middleware

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,11 +1,12 @@
 const router = require("express");
 
-const {getUser, getUsers, createUser, updateUser, deleteUser} = require("../controllers/users.controller")
+const {getUser, getUsers, createUser, updateUser, deleteUser} = require("../controllers/users.controller");
+const { pathMiddleware } = require("../middlewares/pathMiddleware");
 
 
 const userRouter = router();
 
-userRouter.route("/").get(getUsers).get(getUsers).post(createUser);
+userRouter.route("/").get(pathMiddleware, getUsers).get(getUsers).post(createUser);
 userRouter.route("/:userId").get(getUser).patch(updateUser).delete(deleteUser);
 
 module.exports = userRouter;
